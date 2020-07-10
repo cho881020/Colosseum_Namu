b7ac77ba70f3d068f61926518a32fb2f31ed8f13
조경진 <kj_cho@idealidea.co.kr>
2020-07-10 19:50:10 +0900

    로그인 버튼 클릭시 입력값 받기

--- app/src/main/java/kr/co/namu/colosseum/MainActivity.kt
+++ app/src/main/java/kr/co/namu/colosseum/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.co.namu.colosseum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
@@ -13,6 +14,15 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        loginBtn.setOnClickListener {
+//            입력한 아이디 / 비번 받아오기
+            val inputId = idEdt.text.toString()
+            val inputPw = pwEdt.text.toString()
+
+//            서버에 로그인 요청 시도
+
+        }
 
     }
 
