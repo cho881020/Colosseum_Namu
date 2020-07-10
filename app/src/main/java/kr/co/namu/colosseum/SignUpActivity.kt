0d3a45600ace27fe2d9c46ec3f25d508c26372bd
조경진 <kj_cho@idealidea.co.kr>
2020-07-10 21:14:33 +0900

    회원가입 성공/실패 여부 처리. 실패시 실패 사유를 서버가 알려주는대로 출력

--- app/src/main/java/kr/co/namu/colosseum/SignUpActivity.kt
+++ app/src/main/java/kr/co/namu/colosseum/SignUpActivity.kt
@@ -2,6 +2,7 @@ package kr.co.namu.colosseum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_sign_up.*
 import kr.co.namu.colosseum.utils.ServerUtil
 import org.json.JSONObject
@@ -28,6 +29,26 @@ class SignUpActivity : BaseActivity() {
 //            이 입력 항목값들을 서버에 전달해서 회원가입
             ServerUtil.putRequestSignUp(mContext, inputId, inputPw, inputNick, object : ServerUtil.JsonResponseHandler {
                 override fun onResponse(json: JSONObject) {
+
+                    runOnUiThread {
+
+                        val code = json.getInt("code")
+
+                        if (code == 200) {
+                            Toast.makeText(mContext, "회원가입에 성공했습니다.", Toast.LENGTH_SHORT).show()
+                            finish()
+                        }
+                        else {
+//                            가입에 실패하는 이유는 여러가지. (이메일 중복, 닉네임 중복)
+//                            서버가 알려주는 실패 사유를 토스트로 띄우자.
+
+                            val failReason = json.getString("message")
+
+                            Toast.makeText(mContext, failReason, Toast.LENGTH_SHORT).show()
+
+                        }
+
+                    }
 
                 }
 
