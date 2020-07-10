f27bb77024932772a8235524e92323f3f6b6fb54
조경진 <kj_cho@idealidea.co.kr>
2020-07-10 21:03:15 +0900

    회원가입 서버 요청 추가

--- app/src/main/java/kr/co/namu/colosseum/SignUpActivity.kt
+++ app/src/main/java/kr/co/namu/colosseum/SignUpActivity.kt
@@ -2,6 +2,9 @@ package kr.co.namu.colosseum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_up.*
+import kr.co.namu.colosseum.utils.ServerUtil
+import org.json.JSONObject
 
 class SignUpActivity : BaseActivity() {
 
@@ -13,6 +16,24 @@ class SignUpActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+//        회원가입 버튼 처리
+        okBtn.setOnClickListener {
+
+//            입력 항목들 받아오기
+            val inputId = idEdt.text.toString()
+            val inputPw = pwEdt.text.toString()
+            val inputNick = nickNameEdt.text.toString()
+
+//            이 입력 항목값들을 서버에 전달해서 회원가입
+            ServerUtil.putRequestSignUp(mContext, inputId, inputPw, inputNick, object : ServerUtil.JsonResponseHandler {
+                override fun onResponse(json: JSONObject) {
+
+                }
+
+            })
+
+        }
 
     }
 
--- app/src/main/java/kr/co/namu/colosseum/utils/ServerUtil.kt
+++ app/src/main/java/kr/co/namu/colosseum/utils/ServerUtil.kt
@@ -66,6 +66,56 @@ class ServerUtil {
 
         }
 
+//        회원가입 요청을 해주는 함수 => 아이디 / 비번 / 닉네임을 받자.
+
+        fun putRequestSignUp(context: Context, id:String, pw:String, nick:String, handler:JsonResponseHandler?) {
+
+//            서버 통신 담당 변수
+            val client = OkHttpClient()
+
+//            어느 주소로 통신할지
+            val urlStr = "${BASE_URL}/user"
+
+//            서버에 들고갈 짐을 FormData에 담자 (POST / PUT / PATCH에서 이 방식)
+            val formData = FormBody.Builder()
+                .add("email", id)
+                .add("password", pw)
+                .add("nick_name", nick)
+                .build()
+
+//            요청 정보를 종합하는 변수
+            val request = Request.Builder()
+                .url(urlStr)
+                .put(formData)
+                .build()
+
+//            실제 API호출
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+//                    실제 서버 응답이 돌아왔을때 실행됨.
+//                    응답 내용을 저장
+                    val bodyStr = response.body?.string()
+
+//                    이 내용으로 Json객체 생성
+
+                    val json = JSONObject(bodyStr)
+
+                    Log.d("서버 응답 내용", json.toString())
+
+//                    hanlder 변수에 응답처리 코드가 들어있다면 실행
+                    handler?.onResponse(json)
+
+                }
+
+            })
+
+        }
+
+
     }
 
 }
