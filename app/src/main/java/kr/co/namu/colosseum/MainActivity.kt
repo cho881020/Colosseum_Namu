5e3f1044f57ae53aef9a0d174f8fdce36b52bae8
조경진 <kj_cho@idealidea.co.kr>
2020-07-11 18:17:55 +0900

    서버에서 주제 목록 가져와서 ArrayList에 추가

--- app/src/main/java/kr/co/namu/colosseum/MainActivity.kt
+++ app/src/main/java/kr/co/namu/colosseum/MainActivity.kt
@@ -3,11 +3,15 @@ package kr.co.namu.colosseum
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.namu.colosseum.datas.Topic
 import kr.co.namu.colosseum.datas.User
 import kr.co.namu.colosseum.utils.ServerUtil
 import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
+
+    val mTopicList = ArrayList<Topic>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,8 +27,40 @@ class MainActivity : BaseActivity() {
 //        getUserInfoFromServer()
 
 
+        getTopicListFromServer()
 
     }
+
+//    진짜로 서버에서 토론 목록 받아오기
+
+    fun getTopicListFromServer() {
+
+        ServerUtil.getRequestMainInfo(mContext, object : ServerUtil.JsonResponseHandler {
+            override fun onResponse(json: JSONObject) {
+
+                val data = json.getJSONObject("data")
+                val topics = data.getJSONArray("topics")
+
+                for (i in 0..topics.length()-1) {
+
+                    val topicJson = topics.getJSONObject(i)
+
+//                    JSONArray 내부의 {  } JSONObject를 가지고 실제 Topic 객체로 변환
+                    val topic = Topic.getTopicFromJson(topicJson)
+
+//                    바꿔준 topic을 목록에 추가
+                    mTopicList.add(topic)
+
+                }
+
+//                목록을 모두 추가했으면 리스트뷰 새로고침
+
+            }
+
+        })
+
+    }
+
 
 //    임시로 하는 작업. => 서버에서 내 정보를 받아와서 닉네임 뿌려주기
 
--- app/src/main/java/kr/co/namu/colosseum/datas/Topic.kt
+++ app/src/main/java/kr/co/namu/colosseum/datas/Topic.kt
@@ -1,9 +1,25 @@
 package kr.co.namu.colosseum.datas
 
+import org.json.JSONObject
+
 class Topic {
 
     var id = 0
     var title = ""
     var imageUrl = ""
+
+    companion object {
+
+        fun getTopicFromJson(json: JSONObject) : Topic {
+            val topic = Topic()
+
+            topic.id = json.getInt("id")
+            topic.title = json.getString("title")
+            topic.imageUrl = json.getString("img_url")
+
+            return topic
+        }
+
+    }
 
 }
