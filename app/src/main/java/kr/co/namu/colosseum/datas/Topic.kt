2dce580912e2c0a65df514d82bd9ced20cb931d7
조경진 <kj_cho@idealidea.co.kr>
2020-07-11 20:06:13 +0900

    주제 파싱시에 선택 진영 정보도 하위 개념으로 가지고 있도록

--- app/src/main/java/kr/co/namu/colosseum/datas/Topic.kt
+++ app/src/main/java/kr/co/namu/colosseum/datas/Topic.kt
@@ -8,6 +8,8 @@ class Topic {
     var title = ""
     var imageUrl = ""
 
+    val sideList = ArrayList<TopicSide>()
+
     companion object {
 
         fun getTopicFromJson(json: JSONObject) : Topic {
@@ -16,6 +18,20 @@ class Topic {
             topic.id = json.getInt("id")
             topic.title = json.getString("title")
             topic.imageUrl = json.getString("img_url")
+
+//            선택 진영 목록을 파싱해서 => sideList에 담아주기
+
+            val sides = json.getJSONArray("sides")
+
+            for (i in 0..sides.length()-1) {
+
+                val sideJson = sides.getJSONObject(i)
+
+                val topicSide = TopicSide.getTopicSideFromJson(sideJson)
+
+                topic.sideList.add(topicSide)
+
+            }
 
             return topic
         }
