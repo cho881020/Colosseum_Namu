f7313bcd16036a904de8344d885691ae33a7a926
조경진 <kj_cho@idealidea.co.kr>
2020-07-11 18:56:50 +0900

    토론 상세 화면 초안 추가

---
+++ app/src/main/java/kr/co/namu/colosseum/ViewTopicDetailActivity.kt
@@ -0,0 +1,24 @@
+package kr.co.namu.colosseum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewTopicDetailActivity : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_topic_detail)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
