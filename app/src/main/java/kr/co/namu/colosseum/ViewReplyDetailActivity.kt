20bcc6817228830730ae6e737542136dc2d15f75
조경진 <kj_cho@idealidea.co.kr>
2020-07-12 19:39:27 +0900

    답글 목록 화면 추가

---
+++ app/src/main/java/kr/co/namu/colosseum/ViewReplyDetailActivity.kt
@@ -0,0 +1,24 @@
+package kr.co.namu.colosseum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewReplyDetailActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_reply_detail)
+        setupEvents()
+        setValues()
+    }
+
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
