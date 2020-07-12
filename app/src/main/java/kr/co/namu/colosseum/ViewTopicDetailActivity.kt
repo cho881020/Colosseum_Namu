b98d4dd21f77f7bcd464849b383ecbfc072fe1b5
조경진 <kj_cho@idealidea.co.kr>
2020-07-12 18:38:53 +0900

    의견 목록 표시 초안 (일단 띄우기만)

--- app/src/main/java/kr/co/namu/colosseum/ViewTopicDetailActivity.kt
+++ app/src/main/java/kr/co/namu/colosseum/ViewTopicDetailActivity.kt
@@ -2,9 +2,12 @@ package kr.co.namu.colosseum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_view_topic_detail.*
+import kr.co.namu.colosseum.adapters.ReplyAdapter
+import kr.co.namu.colosseum.datas.Reply
 import kr.co.namu.colosseum.datas.Topic
 import kr.co.namu.colosseum.utils.ServerUtil
 import org.json.JSONObject
@@ -16,6 +19,10 @@ class ViewTopicDetailActivity : BaseActivity() {
 
 //    서버에서 받아온 토픽 상세 정보 변수
     lateinit var mTopicData : Topic
+
+    lateinit var mReplyAdapter : ReplyAdapter
+
+    val mReplyList = ArrayList<Reply>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -66,6 +73,10 @@ class ViewTopicDetailActivity : BaseActivity() {
 //        메인에서 보내준 토픽 id값을 저장
         mTopicId = intent.getIntExtra("topicId", 0)
 
+//        리스트뷰와 어댑터 연결
+        mReplyAdapter = ReplyAdapter(mContext, R.layout.reply_list_item, mReplyList)
+        replyListView.adapter = mReplyAdapter
+
 //        서버에서 해당 토픽의 진행상황 / 상세 정보 확인
         getTopicDetailFromServer()
     }
@@ -88,7 +99,23 @@ class ViewTopicDetailActivity : BaseActivity() {
                 for (i in 0..replies.length()-1) {
 //                    의견 하나하나를 Replies 형태로 변환.
 
+                    val replyJson = replies.getJSONObject(i)
+
+                    val reply = Reply.getReplyFromJson(replyJson)
+
+//                    만들어낸 의견을 목록에 추가
+                    mReplyList.add(reply)
+
                 }
+
+                runOnUiThread {
+
+//                의견이 모두 추가되었으면 어댑터 새로고침
+                    mReplyAdapter.notifyDataSetChanged()
+
+                }
+
+
 
                 runOnUiThread {
                     titleTxt.text = mTopicData.title
